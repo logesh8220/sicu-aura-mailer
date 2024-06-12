441deb160e96fe6e1f8a611fcb6939263923ffe0
logesh8220 <logeshthirumurugan@gmail.com>
2024-06-12 22:02:37 +0530

    added multiple mail system

---
+++ README.md
@@ -0,0 +1,2 @@
+# Node-Mailer
+Mailer
---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = 3000;
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'websites.sicu.aura@gmail.com',
+    pass: process.env.PASS
+  }
+});
+
+app.use(express.json());
+app.use(cors());
+
+// POST endpoint 
+app.post('/sendEmail', (req, res) => {
+  const { to, subject, text, html, header } = req.body;
+
+
+  const recipients = to.split(',').map(email => email.trim());
+
+  const mailOptions = {
+    from: `${header} <websites.sicu.aura@gmail.com>`,
+    to: recipients.join(','), 
+    subject: subject,
+    cc: 'sicu.aura.official@gmail.com',
+    text: text,
+    html: html,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.error('Error:', error);
+      res.status(500).send('Failed to send email');
+    } else {
+      console.log('Email sent:', info.response);
+      res.status(200).send('Email sent successfully');
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
